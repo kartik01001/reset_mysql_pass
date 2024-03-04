2a6c50d1b8f7e0a2b446050db7f308c489e3d31d
Kartik Panwar <73104035+kartik01001@users.noreply.github.com>
2024-03-04 11:33:11 +0530

    Update

--- reset_pass.cmd
+++ reset_pass.cmd
@@ -1,8 +1,20 @@
 cd c:\
 echo ALTER USER 'root'@'localhost' IDENTIFIED BY '1234'; > reset.txt
-cd C:\Program Files\MySQL\MySQL Server 8.3\bin
+cd C:\Program Files\MySQL\MySQL Server*\bin
 net stop MySQL83
-start mysqld --defaults-file="C:\\ProgramData\\MySQL\\MySQL Server 8.3\\my.ini" --init-file=C:\reset.txt
+
+set "mysql_ini="
+for /D %%d in ("C:\ProgramData\MySQL\MySQL Server *") do (
+    if exist "%%d\my.ini" (
+        set "mysql_ini=%%d\my.ini"
+        goto :found
+    )
+)
+echo Error: MySQL configuration file not found.
+exit /b 1
+
+:found
+start mysqld.exe --defaults-file="%mysql_ini%" --init-file=C:\reset.txt
 
 timeout 5 > NUL
 
